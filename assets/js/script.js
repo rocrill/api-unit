2e309c1b94ec172c59c34989252a369ae8fbef42
rocrill <crillyr@tcd.ie>
2021-12-29 17:13:40 +0000

    Initial js commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const API_KEY = "OTDEdvsQVBhaW7jEGS5Bv9aMz3U";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+
+}
